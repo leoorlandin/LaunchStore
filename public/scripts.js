e016092fcfd2628a4d051838d8f13bc560e937ab
unknown <leonardo.1117@hotmail.com>
2020-07-25 05:58:17 -0300

    add logic for remove photos on the create page

--- public/scripts.js
+++ public/scripts.js
@@ -17,15 +17,11 @@ const Mask = {
 
 const PhotosUpload = {
   uploadLimit: 6,
+  preview: document.querySelector('#photos-preview'),
   handleFileInput(event) {
     const { files: fileList } = event.target
-    const { uploadLimit } = PhotosUpload
 
-    if(fileList.length > uploadLimit) {
-      alert(`Por favor, envie no máximo ${uploadLimit} fotos do produto`)
-      event.preventDefault()
-      return
-    }
+    if(PhotosUpload.hasLimit(event)) return
 
     Array.from(fileList).forEach(file => {
       const reader = new FileReader()
@@ -34,18 +30,50 @@ const PhotosUpload = {
         const image = new Image()
         image.src = String(reader.result)
 
-        const div = document.createElement('div')
-        div.classList.add('photo')
+        const div = PhotosUpload.getContainer(image)
 
-        div.onclick = () => alert('excluir imagem')
-
-        div.appendChild(image)
-
-        document.querySelector('#photos-preview').appendChild(div)
+        PhotosUpload.preview.appendChild(div)
       }
 
       reader.readAsDataURL(file)
     })
 
+  },
+  getContainer(image) {
+    const div = document.createElement('div')
+    div.classList.add('photo')
+
+    div.onclick = PhotosUpload.removePhoto
+
+    div.appendChild(image)
+
+    div.appendChild(PhotosUpload.getRemoveButton())
+
+    return div
+  },
+  hasLimit(event) {
+    const { uploadLimit } = PhotosUpload
+    const { files: fileList } = event.target
+
+    if (fileList.length > uploadLimit) {
+      alert(`Por favor, envie no máximo ${uploadLimit} fotos do produto`)
+      event.preventDefault()
+      return true
+    }
+    return false
+  },
+  getRemoveButton(){
+    const button = document.createElement('i')
+    button.classList.add('material-icons')
+
+    button.innerHTML = 'close'
+    return button
+  },
+  removePhoto(event){
+    const PhotoDiv = event.target.parentNode
+    const photosArray = Array.from(PhotosUpload.preview.children)
+    const index = photosArray.indexOf(PhotoDiv)
+
+    PhotoDiv.remove()
   }
 }
