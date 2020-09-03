85ba4ab4db8b38640b16f741d9c97f6ef88feb41
unknown <leonardo.1117@hotmail.com>
2020-09-03 15:17:13 -0300

    add dependencie//add logic to upload files in DB

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -2,6 +2,7 @@ const { formatPrice } = require('../../lib/utils')
 
 const Category = require('../models/Category')
 const Product = require('../models/Product')
+const File = require('../models/File')
 
 module.exports = {
   create(req, res) {
@@ -26,10 +27,21 @@ module.exports = {
       }
     }
 
+    if (req.files.length == 0) {
+      return res.send("Please send at least one image")
+    }
+
     let results = await Product.create(req.body)
     const productId = results.rows[0].id
 
-    return res.redirect(`products/${productId}/edit`)
+    const filesPromise = req.files.map(file => File.create({
+      ...file,
+      product_id: productId
+    }))
+
+    await Promise.all(filesPromise)
+
+    return res.redirect(`products/${productId}`)
 
   },
   async edit(req, res) {
