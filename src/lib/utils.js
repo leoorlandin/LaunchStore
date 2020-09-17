7b3e9fca4c107041f77b62abeb57482881ddfc03
unknown <leonardo.1117@hotmail.com>
2020-09-17 08:54:17 -0300

    finished product show page styles//created logic for show info saved on DB for each product on show page

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -7,11 +7,15 @@ module.exports = {
     const year = date.getUTCFullYear()
     const month = `0${date.getUTCMonth() + 1}`.slice(-2)
     const day = `0${date.getUTCDate()}`.slice(-2)
+    const hour = date.getHours()
+    const minutes = date.getMinutes()
 
     return {
       day,
       month,
       year,
+      hour,
+      minutes,
       iso: `${year}-${month}-${day}`,
       birthDay: `${day}/${month}`,
       format: `${day}/${month}/${year}`
